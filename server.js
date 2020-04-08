c62dc9e4513d42fe83535320e0c278e793eba55f
tsjuusmei <guusmay2@hotmail.com>
2020-04-08 22:36:14 +0200

    added app.use bodyparser lines

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 
 const app = express();
 
-
 var usersRouter = require('./users');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
